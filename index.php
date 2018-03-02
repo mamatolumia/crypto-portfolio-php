16c6c55d6f2ec28ab367e1241ac11d407c929fd8
hjonathan87 <hjonathan87@outlook.com>
2018-03-02 10:25:09 -0500

    Added poloniex API functionality and created all totals.

--- index.php
+++ index.php
@@ -23,6 +23,7 @@ $miningpoolhubUSDUnconfirmed = Array ();
 
 
 $priceInUSD = Array ();
+$priceInUSDOther = 0;
 
 $coinInUSDInMiningpoolhub = Array ();
 
@@ -31,7 +32,19 @@ $coinInUSDInMiningpoolhub['ETN'] = 0;
 $coinInUSDInMiningpoolhub['VTC'] = 0;
 $coinInUSDInMiningpoolhub['ZEC'] = 0;
 
+$coinInUSDInMiningpoolhubUnconfirmed = Array ();
+
+$coinInUSDInMiningpoolhubUnconfirmed['DGB'] = 0;
+$coinInUSDInMiningpoolhubUnconfirmed['ETN'] = 0;
+$coinInUSDInMiningpoolhubUnconfirmed['VTC'] = 0;
+$coinInUSDInMiningpoolhubUnconfirmed['ZEC'] = 0;
+
 $totalInUSDInMiningpoolhub = 0;
+$totalInUSDInMiningpoolhubUnconfirmed = 0;
+$totalInUSDInMiningpoolhubPlusUnconfirmed = 0;
+
+
+$poloniexUSDTotal = 0;
 
 $totalInUSD = 0;
 
@@ -176,8 +189,6 @@ function coinpayments_api_call_rates($cmd, $req = array()) {
 
 
 print_r(coinpayments_api_call_rates('rates'));
-
-echo "<br>" . "<br>";
 
 
 
@@ -344,12 +355,21 @@ function cryptocompare_api_call($cmd, $req = array()) {
 
 
            $localcoinInUSDInMiningpoolhub = Array ("DGB"=>0, "ETN"=>0, "VTC"=>0, "ZEC"=>0);
+           $localcoinInUSDInMiningpoolhubUnconfirmed = Array ("DGB"=>0, "ETN"=>0, "VTC"=>0, "ZEC"=>0);
 
            $GLOBALS['priceInUSD[DGB]'] = $dec['DGB']['USD'];
            $GLOBALS['priceInUSD[ETN]'] = $dec['ETN']['USD'];
            $GLOBALS['priceInUSD[VTC]'] = $dec['VTC']['USD'];
            $GLOBALS['priceInUSD[ZEC]'] = $dec['ZEC']['USD'];
-           // $GLOBALS['priceInUSD[XRP]'] = $dec['XRP']['USD'];
+
+
+
+
+           $GLOBALS['priceInUSDOther'] = $dec['XRP']['USD'];
+
+
+
+
 
             $localPriceInUSD['DGB'] = $GLOBALS['priceInUSD[DGB]'];
             $localPriceInUSD['ETN'] = $GLOBALS['priceInUSD[ETN]'];
@@ -370,18 +390,21 @@ function cryptocompare_api_call($cmd, $req = array()) {
 
 
 
-
             foreach($localPriceInUSD as $x => $x_value) {
 
 
 
 
-                $localcoinInUSDInMiningpoolhub[$x] = $x_value * $GLOBALS['miningpoolhubUSD'][$x] . "<br>";
-                echo $localcoinInUSDInMiningpoolhub[$x];
+                $localcoinInUSDInMiningpoolhub[$x] = $x_value * $GLOBALS['miningpoolhubUSD'][$x];
+                $localcoinInUSDInMiningpoolhubUnconfirmed[$x] = $x_value * $GLOBALS['miningpoolhubUSDUnconfirmed'][$x];
+
+                echo $x . " in USD Confirmed" . " " . $localcoinInUSDInMiningpoolhub[$x]  . "<br>" . $x . " in USD Unconfirmed" . " " . $localcoinInUSDInMiningpoolhubUnconfirmed[$x]  . "<br>" . "<br>";
 
 
 
                 $GLOBALS['totalInUSDInMiningpoolhub'] = $GLOBALS['totalInUSDInMiningpoolhub'] + ($x_value * $GLOBALS['miningpoolhubUSD'][$x]);
+                $GLOBALS['totalInUSDInMiningpoolhubUnconfirmed'] = $GLOBALS['totalInUSDInMiningpoolhubUnconfirmed'] + ($x_value * $GLOBALS['miningpoolhubUSDUnconfirmed'][$x]);
+
 
                 // echo $GLOBALS['totalInUSD'][$x];
                 // $localPriceInUSD[$x] = $x_value * $localMiningpoolhubUSD[$x];
@@ -391,14 +414,11 @@ function cryptocompare_api_call($cmd, $req = array()) {
                 // echo "<br>";
             }
 
+            $GLOBALS['totalInUSDInMiningpoolhubPlusUnconfirmed'] = $GLOBALS['totalInUSDInMiningpoolhub'] + $GLOBALS['totalInUSDInMiningpoolhubUnconfirmed'];
 
 
 
-
-             echo $GLOBALS['totalInUSDInMiningpoolhub'];
-
-
-
+             echo "Total USD on Miningpoolhub Confirmed " . $GLOBALS['totalInUSDInMiningpoolhub'] . "<br>" . "Total USD on Miningpoolhub Unconfirmed " . $GLOBALS['totalInUSDInMiningpoolhubUnconfirmed'] . "<br>" . "<br>" . "Total USD on Miningpoolhub including Unconfirmed " . $GLOBALS['totalInUSDInMiningpoolhubPlusUnconfirmed'] . "<br>" . "<br>";
 
 
             // return $dec;
@@ -416,8 +436,243 @@ function cryptocompare_api_call($cmd, $req = array()) {
 
 cryptocompare_api_call('test');
 
+// FINAL TESTED CODE - Created by Compcentral
 
+// NOTE: currency pairs are reverse of what most exchanges use...
+//       For instance, instead of XPM_BTC, use BTC_XPM
 
+class poloniex {
+    protected $api_key;
+    protected $api_secret;
+    protected $trading_url = "https://poloniex.com/tradingApi";
+    protected $public_url = "https://poloniex.com/public";
+
+    public function __construct($api_key, $api_secret) {
+        $this->api_key = $api_key;
+        $this->api_secret = $api_secret;
+    }
+
+    private function query(array $req = array()) {
+        // API settings
+        $key = $this->api_key;
+        $secret = $this->api_secret;
+
+        // generate a nonce to avoid problems with 32bit systems
+        $mt = explode(' ', microtime());
+        $req['nonce'] = $mt[1].substr($mt[0], 2, 6);
+
+        // generate the POST data string
+        $post_data = http_build_query($req, '', '&');
+        $sign = hash_hmac('sha512', $post_data, $secret);
+
+        // generate the extra headers
+        $headers = array(
+            'Key: '.$key,
+            'Sign: '.$sign,
+        );
+
+        // curl handle (initialize if required)
+        static $ch = null;
+        if (is_null($ch)) {
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_USERAGENT,
+                'Mozilla/4.0 (compatible; Poloniex PHP bot; '.php_uname('a').'; PHP/'.phpversion().')'
+            );
+        }
+        curl_setopt($ch, CURLOPT_URL, $this->trading_url);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $post_data);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+
+        // run the query
+        $res = curl_exec($ch);
+
+        if ($res === false) throw new Exception('Curl error: '.curl_error($ch));
+        //echo $res;
+        $dec = json_decode($res, true);
+        if (!$dec){
+            //throw new Exception('Invalid data: '.$res);
+            return false;
+        }else{
+            return $dec;
+        }
+    }
+
+    protected function retrieveJSON($URL) {
+        $opts = array('http' =>
+            array(
+                'method'  => 'GET',
+                'timeout' => 10
+            )
+        );
+        $context = stream_context_create($opts);
+        $feed = file_get_contents($URL, false, $context);
+        $json = json_decode($feed, true);
+        return $json;
+    }
+
+    public function get_balances() {
+        return $this->query(
+            array(
+                'command' => 'returnBalances'
+            )
+        );
+    }
+
+    public function get_open_orders($pair) {
+        return $this->query(
+            array(
+                'command' => 'returnOpenOrders',
+                'currencyPair' => strtoupper($pair)
+            )
+        );
+    }
+
+    public function get_my_trade_history($pair) {
+        return $this->query(
+            array(
+                'command' => 'returnTradeHistory',
+                'currencyPair' => strtoupper($pair)
+            )
+        );
+    }
+
+    public function buy($pair, $rate, $amount) {
+        return $this->query(
+            array(
+                'command' => 'buy',
+                'currencyPair' => strtoupper($pair),
+                'rate' => $rate,
+                'amount' => $amount
+            )
+        );
+    }
+
+    public function sell($pair, $rate, $amount) {
+        return $this->query(
+            array(
+                'command' => 'sell',
+                'currencyPair' => strtoupper($pair),
+                'rate' => $rate,
+                'amount' => $amount
+            )
+        );
+    }
+
+    public function cancel_order($pair, $order_number) {
+        return $this->query(
+            array(
+                'command' => 'cancelOrder',
+                'currencyPair' => strtoupper($pair),
+                'orderNumber' => $order_number
+            )
+        );
+    }
+
+    public function withdraw($currency, $amount, $address) {
+        return $this->query(
+            array(
+                'command' => 'withdraw',
+                'currency' => strtoupper($currency),
+                'amount' => $amount,
+                'address' => $address
+            )
+        );
+    }
+
+    public function get_trade_history($pair) {
+        $trades = $this->retrieveJSON($this->public_url.'?command=returnTradeHistory&currencyPair='.strtoupper($pair));
+        return $trades;
+    }
+
+    public function get_order_book($pair) {
+        $orders = $this->retrieveJSON($this->public_url.'?command=returnOrderBook&currencyPair='.strtoupper($pair));
+        return $orders;
+    }
+
+    public function get_volume() {
+        $volume = $this->retrieveJSON($this->public_url.'?command=return24hVolume');
+        return $volume;
+    }
+
+    public function get_ticker($pair = "ALL") {
+        $pair = strtoupper($pair);
+        $prices = $this->retrieveJSON($this->public_url.'?command=returnTicker');
+        if($pair == "ALL"){
+            return $prices;
+        }else{
+            $pair = strtoupper($pair);
+            if(isset($prices[$pair])){
+                return $prices[$pair];
+            }else{
+                return array();
+            }
+        }
+    }
+
+    public function get_trading_pairs() {
+        $tickers = $this->retrieveJSON($this->public_url.'?command=returnTicker');
+        return array_keys($tickers);
+    }
+
+    public function get_total_btc_balance() {
+        $balances = $this->get_balances();
+        $prices = $this->get_ticker();
+
+        $tot_btc = 0;
+
+        foreach($balances as $coin => $amount){
+            $pair = "BTC_".strtoupper($coin);
+
+            // convert coin balances to btc value
+            if($amount > 0){
+                if($coin != "BTC"){
+                    $tot_btc += $amount * $prices[$pair];
+                }else{
+                    $tot_btc += $amount;
+                }
+            }
+
+            // process open orders as well
+            if($coin != "BTC"){
+                $open_orders = $this->get_open_orders($pair);
+                foreach($open_orders as $order){
+                    if($order['type'] == 'buy'){
+                        $tot_btc += $order['total'];
+                    }elseif($order['type'] == 'sell'){
+                        $tot_btc += $order['amount'] * $prices[$pair];
+                    }
+                }
+            }
+        }
+
+        return $tot_btc;
+    }
+}
+
+$poloniex_api_key = file_get_contents('./api keys/poloniexapikey.txt');
+$poloniex_secret = file_get_contents('./api keys/poloniexsecret.txt');
+
+// change parameters in object declaration to api key and secret in quotes
+
+$polo = new poloniex($poloniex_api_key, $poloniex_secret);
+
+$poloniexBalance = $polo->get_balances();
+
+echo "Ripple " . $poloniexBalance['XRP'] . "<br>";
+
+// print_r($poloniexBalance);
+
+// var_dump($poloniexBalance);
+
+$poloniexUSDTotal = $poloniexBalance['XRP'] * $priceInUSDOther;
+
+echo "Total in USD in Poloniex " . $poloniexUSDTotal . "<br>" . "<br>";
+
+$totalInUSD = $coinpaymentsTotalUSD + $totalInUSDInMiningpoolhub + $poloniexUSDTotal;
+
+echo "Total in USD " . $totalInUSD;
 
 
 
